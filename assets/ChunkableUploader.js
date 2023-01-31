bea759b7a28564579f42d33456e22ed7513b1a02
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2023-01-31 13:35:26 +0000

    Remove debug

--- assets/ChunkableUploader.js
+++ assets/ChunkableUploader.js
@@ -10,25 +10,17 @@ class ChunkableUploader {
         for(var item in data) {
             this[item] = data[item];
         }
-
-        // console.log(this);
+        
     }
 
 
     upload(file) {
         
-        // console.log(file.name);
-
         this.file = file;
 
         // work out how mnay chunks (rounded up)
         this.chunkCount = Math.ceil(file.size / this.chunkSize);
-        // let i = 0;
-        // while( i < this.chunkCount) {
-        //     this.makeChunk(i);
-        //     i++;
-        // }   
-
+    
 
      // ** Uncomment from here to enable file checksumming **    
     //     let uploader = this;
@@ -67,8 +59,6 @@ class ChunkableUploader {
 
 
     uploadChunk(idx=1) {
-
-        // console.log('Starting chunk ' + idx);
 
         if(idx > this.chunkCount) {
             throw new Error('Chunk ' + idx + ' out of bounds');
@@ -123,18 +113,20 @@ class ChunkableUploader {
 
                 xhr.upload.addEventListener("error", function(evt) {
                     console.log('Upload Error', evt);
+                    $(document).trigger('chunkupload-error', {
+                        chunkerId: uploader.chunkerId,
+                        message: "An error occurred during upload."
+                    });
                 });
                 
                 //Upload progress
                 xhr.upload.addEventListener("progress", function(evt){
-                // xhr.onprogress = function(evt) {
-
-                    console.log(evt);
             
                     // calc overall percentage:
                     let prevChunks = uploader.chunkSize * (idx-1);
                     let loaded = (prevChunks + evt.loaded);
 
+                    // throw a new event
                     $(document).trigger('chunkupload-progress', {
                         chunkerId: uploader.chunkerId,
                         percentComplete: (loaded / uploader.file.size) * 100,
@@ -143,8 +135,7 @@ class ChunkableUploader {
                         filename: uploader.file.name
                     });
 
-                    // }
-                }); //, false);
+                });
                 return xhr;
             },
             cache: false,
@@ -172,8 +163,6 @@ class ChunkableUploader {
                   
             }).fail(function (data) {
 
-                // console.log('ERROR', data);
-
                 let message = ''
 
                 switch(data.status) {
@@ -182,12 +171,10 @@ class ChunkableUploader {
                     break;
 
                     case 413:
-                        // alert('The file is too large for the server to accept');
                         message = 'The file is too large for the server to accept'; 
                     break;
 
                     default:
-                        // alert('An unexpected error occurred');
                         message = 'An unexpected error occurred';
                         break;
                 }
@@ -198,10 +185,6 @@ class ChunkableUploader {
                     status: data.status,
                     message: message
                 });
-
-                //     
-    
-                //     // self.reset();
     
             });
 
@@ -212,13 +195,9 @@ class ChunkableUploader {
         let chunkStart = this.chunkSize * (idx-1);
         let chunkEnd = Math.min(chunkStart + this.chunkSize , this.file.size );
 
-        // console.log('chunking: ', idx, chunkStart, chunkEnd);
-
         let chunk = this.file.slice(chunkStart, chunkEnd);
 
         return chunk;
-
-        // console.log(chunk)
 
     }
 
