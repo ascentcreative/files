adb5351bfe401c2c80774c3036a0f7e538db73d4
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2025-01-28 11:05:05 +0000

    Fudge to handle string nulls (need to prevent these really)

--- resources/views/components/file-link.blade.php
+++ resources/views/components/file-link.blade.php
@@ -1,4 +1,4 @@
 <a class="file-link" href="{{ route('file.stream', ['file'=>$file]) }}" target="_blank">
-    <span class="file-label">{{ $file->label ? $file->label : $file->original_filename }}</span>
+    <span class="file-label">{{ ($file->label && $file->label != 'null') ? $file->label : $file->original_filename }}</span>
     <span class="file-size">{{ $file->sizeHuman }}</span>
 </a>
